91b0de62d682dc548cfb10d56a67cfe06644055b
Tetiana Kovalenko <tkovaletc@training.local>
2019-08-16 15:54:37 +0300

    first commit

---
+++ src/app/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div className="working-test">
+      Working
+  </div>
+);
+
+export default App;
---
+++ webpack.config.dev.babel.js
@@ -0,0 +1,107 @@
+import '@babel/polyfill';
+
+import webpack from 'webpack';
+
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+import autoprefixer from 'autoprefixer';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: [
+      '@babel/polyfill',
+      './src/app/index.js',
+      'bootstrap/dist/css/bootstrap.min.css',
+      './src/public/styles/scss/main.scss',
+    ],
+    bundle: ['bootstrap'],
+  },
+  output: {
+    filename: '[name].js',
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000, // How often check for changes (in milliseconds)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react'],
+            cacheDirectory: true,
+            plugins: ['react-hot-loader/babel'],
+          },
+        }],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                autoprefixer,
+              ],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.(png|gif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+          'img-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/public/img', to: 'img' },
+    ]),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].js.map',
+      exclude: ['bundle.js'],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './src/index.html',
+    }),
+  ],
+};
