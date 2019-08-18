21b818e665258075f35ef4da153170d839bf1e39
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-08-18 23:05:36 +0300

    adde search box style

--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Hotels from '../../containers/hotels';
+import './home.scss';
 
 const Home = () => {
     return (
         <div>
+            <div className="searchbox col-lg-6 col-md-6 col-sm-12 m-auto">
+            <form className="form-inline">
+                <input className="form-control flex-grow-1 mr-sm-2" type="search" 
+                        placeholder="Шукати..." aria-label="Search" name="name" />
+                <button className="btn button-dark my-2 my-sm-0" type="submit">Шукати</button>
+            </form>
+            </div>
             <Hotels/>
         </div>
     )
